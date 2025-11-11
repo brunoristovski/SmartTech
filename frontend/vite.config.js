2655adf86be204cabf2bf4bd1eb7317d63636f62
Bruno Ristovski <brunoristovski@gmail.com>
2025-11-11 20:41:05 +0100

    small update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3000,       // change the port here
+        port: 3000,
     },
 });
