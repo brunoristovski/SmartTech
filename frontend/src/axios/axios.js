680a166bf3e68b94b0af84b41cdebf69d740368d
Bruno Ristovski <brunoristovski@gmail.com>
2025-11-12 11:48:24 +0100

    Created jwt Authorization in React

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -5,6 +5,24 @@ const axiosInstance = axios.create({
     headers: {
         "Content-Type": "application/json",
     }
-})
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const jwtToken = localStorage.getItem("token");
+        if (jwtToken) {
+            config.headers.Authorization = `Bearer ${jwtToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+            console.log("Invalid token");
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    },
+);
 
 export default axiosInstance
---
+++ frontend/src/contexts/authContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+//kontekst za pregled na logirani korisnici(avtentikacija)
+
+const authContext = React.createContext(null);
+
+export default authContext;
